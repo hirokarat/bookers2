333feb34f67fa5393c25808246f68ada1d0802e4
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-07-30 08:21:38 +0000

    フラッシュメッセージまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to books_path,notice:'You have created book successfully.'
+    else
+      render:index
+    end
   end
 
   def index
@@ -28,8 +31,9 @@ class BooksController < ApplicationController
   
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+       redirect_to book_path(@book.id),notice:'You have updated book successfully.'
+    end
   end
   
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users=User.all
+     @user=User.find(current_user.id)
   end
   
   def show
@@ -14,13 +15,14 @@ class UsersController < ApplicationController
   
   def update
     @user=User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+       redirect_to user_path(@user.id),notice:'You have updated user successfully.'
+    end
   end
   
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction,:profile_book)
+    params.require(:user).permit(:name, :introduction,:profile_image)
   end
 end
